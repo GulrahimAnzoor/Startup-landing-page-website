46d23ef549336a188b96207a51a6901cf266fa26
GulrahimAnzoor <gulrahimanoor201@gmail.com>
2024-12-15 09:44:38 +0430

    Startup landing website

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Headar from "./StartUp/hedar/Headar";
+import Hero from "./StartUp/hero/Hero";
+import Service from "./StartUp/service/Service";
+import Watch from "./StartUp/watch/Watch";
+import Team from "./StartUp/team/Team";
+import FAQ from "./StartUp/faq/FAQ";
+import Pricing from "./StartUp/pricing/Pricing";
+import Start from "./StartUp/starts/Start";
+import Testimoials from "./StartUp/testimonials/Testimonials";
+import Contact from "./StartUp/contact/Contact";
+import Footer from "./StartUp/footer/Footer";
+import DarkMood from "./StartUp/darkmood/DarkMood";
+// ----------------
+ export default function App() {
+  return (
+  <>
+
+<Headar />
+<Hero />
+<Service />
+<Watch />
+<Pricing />
+<Team />
+<FAQ />
+<Start />
+<Testimoials />
+<Contact />
+<Footer />
+<DarkMood />
+  </>
+  );
+} 
